9d2a593170fff04d6d28020660c45ac43133f55f
William <bluedragongxx@gmail.com>
2026-03-19 15:35:19 +0000

    image

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Page() {
   return (
     <main>
         <NavBar />
-        <div>herro</div>
+        <div>
+            <Image src="/homepage.png" height={"1330"} width={"2000"} alt="background photo" />
+        </div>
     </main>
   );
 }
