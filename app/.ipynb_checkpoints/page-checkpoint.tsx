689d28448f7ba931697700f3e5c8fcef0db34e58
William <bluedragongxx@gmail.com>
2026-03-21 06:23:37 +0000

    good

--- app/.ipynb_checkpoints/page-checkpoint.tsx
+++ app/.ipynb_checkpoints/page-checkpoint.tsx
@@ -6,9 +6,12 @@ export default function Page() {
   return (
     <main>
         <NavBar />
-        <Image src="/homepage.png" height={"1330"} width={"2000"} alt="background photo" className="w-full overflow-hidden bg-center fixed left-0 -top-55 z-0 filter brightness-50" />
+        <div className="z-0" >
+            <Image src="/homepage.png" height={"1330"} width={"2000"} alt="hero photo" className="w-full overflow-hidden bg-center absolute left-0 -top-55 z-0 filter brightness-50" />
+        </div>
         
-        <div className="h-screen w-screen absolute z-10 font-light" style={{padding:"15% 0 25% 0"}}>
+        
+        <div className="h-screen w-screen absolute z-10 font-light" style={{margin:"15% 0 25% 0"}}>
             <h1 className="place-self-center italic m-10 py-10 text-6xl md:py-none md:text-9xl">
                 Feelings are important, but
                 it's the physics that matters.
