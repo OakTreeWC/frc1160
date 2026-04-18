fc0585fa8c507d00057a638dd4289768b782403c
William <bluedragongxx@gmail.com>
2026-04-18 05:58:14 +0000

    the overlord

--- app/cabinet/navbar2.tsx
+++ app/cabinet/navbar2.tsx
@@ -7,7 +7,8 @@ export default function Navbar2() {
   const pathname = usePathname();
   const links = [
       {"name":"Engineering","href":"/cabinet/engineering"},
-      {"name":"Business", "href":"/cabinet/business"}
+      {"name":"Business", "href":"/cabinet/business"},
+      {"name":"The Overlord", "href":"/cabinet/overlord"}
   ]
   return (
     <main className = {`flex flex-row space-x-0 justify-left items-center md:p-0  md:h-25 absolute top-25 z-10 md:bg-transparent w-full`}>
