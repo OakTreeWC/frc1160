dfa8e240062d0b72c143b0664eaab84377b8afb0
William <bluedragongxx@gmail.com>
2026-05-21 18:39:14 +0000

    I dont remember

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default function Custom404() {
       <div className="bg-gray-200 flex flex-col-reverse justify-between md:flex-row gap-5 p-10 md:px-50 m-10 w-full md:h-100 rounded-2xl">
         <div className="flex flex-col text-4xl justify-start gap-5 text-center grow text-black m-10">
             <h1 className="text-7xl font-medium">404 Not Found</h1>
-            <span>click this guy to go home</span>
+            <span>Wuh Woh. That doesn't exist.</span>
         </div>
         <Link href="/"><Image src="/404/error404.png" className="w-full h-full object-contain" width={300} height={300} loading="eager" alt="404 Error"></Image></Link>
       </div>
